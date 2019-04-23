35afb5c610ee0e47fcfa191959e349d6d9044d3e
Stijn Polfliet <spolfliet@newrelic.com>
2019-04-23 15:02:02 +0200

    Add DynamoDB functionality

--- app/worker/index.js
+++ app/worker/index.js
@@ -1,3 +1,8 @@
+var AWS = require('aws-sdk');
+var db = new AWS.DynamoDB({
+    region: 'eu-west-1'
+});
+
 // Worker Lambda function listens to guestbook-parser queue
 exports.handler = (event, context, callback) => {
     // Process each SQS event
@@ -7,7 +12,19 @@ exports.handler = (event, context, callback) => {
         // Transform the message
         console.log('Worker received: ' + message);
         
-        // TODO send to Elasticache
+        // Store message in DynamoDB
+        var params = {
+            TableName: 'GUESTBOOK_MESSAGES',
+            Item: {
+                'MESSAGE' : {S: message}
+            }
+        };
+        console.log('Worker saving to DynamoDB');
+        db.putItem(params, function(err, data) {
+            if (err) {
+                console.log('Worker error', err);
+            }
+        });
   });
 
   return {};
